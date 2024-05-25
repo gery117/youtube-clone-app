b0bac934bd27f65dadf698ac49da647d75133c6c
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-05-26 01:30:26 +0800

    created routes paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Box } from '@mui/material';
 
-const App = () => {
-  return (
-    <div>App</div>
+const App = () => (
+    <BrowserRouter>
+      <Box sx={{backgroundColor: '#000'}}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" exact element={<Feed/>}/>
+          <Route path="/video/:id" element={<VideoDetail/>}/>
+          <Route path="/" element={<ChannelDetail/>}/>
+          <Route path="/" element={<SearchFeed/>}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
-}
+
 
 export default App
