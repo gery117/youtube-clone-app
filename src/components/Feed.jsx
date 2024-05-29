27e30fc7d6397af4132d24e4dc4c98996cca2275
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-05-29 15:41:56 +0800

    created fetchfromapi component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,6 +2,8 @@ import {useState, useEffect} from 'react';
 import {Box, Stack, Typography} from '@mui/material'
 // import Sidebar from './Sidebar';
 // import Videos from './Videos';
+
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos} from './'
 
 const Feed = () => {
