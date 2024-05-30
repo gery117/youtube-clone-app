bd706ec74d2fb902adfdb8497abb5a79fa4a8548
Geraldo Kranenburg <geraldo.kranenburg@outlook.com>
2024-05-30 17:07:09 +0800

    added channelCard

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -7,10 +7,12 @@ import { fetchFromAPI } from '../utils/fetchFromAPI';
 import { Sidebar, Videos} from './'
 
 const Feed = () => {
-  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([])
 
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
   }, [selectedCategory])
   
 
@@ -48,7 +50,7 @@ const Feed = () => {
         <Typography variant='h4' fontWeight= 'bold' mb={2} sx={{color: 'white'}}>
           {selectedCategory} <span style= {{color: '#F31503'}}>videos</span>
         </Typography>
-        <Videos videos={[]}/>
+        <Videos videos={videos}/>
       </Box>
     </Stack>
   )
